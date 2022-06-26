513b65c3f1ea55b214b772d6781b9db321f6a924
AppropriateThreat <paulflorentin.pf@gmail.com>
2022-06-26 14:01:28 +0300

    new files added

--- scripts.js
+++ scripts.js
@@ -2,3 +2,11 @@ var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     document.querySelector("body").classList.toggle("active");
 })
+
+const imageUrl = "https://media-exp2.licdn.com/dms/image/C4D03AQHQTMhVT8pp_A/profile-displayphoto-shrink_800_800/0/1652970966451?e=1661990400&v=beta&t=FN92by-znwGiFOLIkhhZQvQsNWyMxj8PGCCU4HcF-os";
+fetch(imageUrl)
+    .then(response => response.blob())
+    .then(imageBlob => {
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        console.log(imageObjectURL);
+    });
